ba346c32c4bdfd36013db5e132a12fd5a5d8ff8d
Ben <benedikthuber44@googlemail.com>
2018-02-25 18:28:13 +0100

    finished

--- client.py
+++ client.py
@@ -8,43 +8,21 @@ import time
 import threading
 import sys
 
-class Producer:
-    def __init__(self):
-	    print "start Producer.." 
-
-    def start(self, clientsocket, queue):
-    	while True:
-            message = clientsocket.recv(1024)
-            queue.put(message) 
-            if message == '0\r\n': break
-
-class Consumer():
+class Torrent():
     def __init__(self):
         print "start Consumer.."
 
-    def start(self, clientsocket, queue):
-        while True:
-            if not queue.empty():
-                assigment = queue.get()
-                if assigment == '0\r\n': 
-                    break
-                elif str.startswith(assigment, "magnet:?"):
-                    torrent_file = self.convert(assigment, "torrent_tmp")
-                    self.download(torrent_file, clientsocket)
-                else:
-                    print("got broken magnet..")
-                
-            print "checking queue.."
-            time.sleep(1)
+    def send(self, socket, message):
+        socket.send(message + "\n")
 
-    def convert(self, magnet, output_name=None):
-        if output_name and \
-                not pt.isdir(output_name) and \
-                not pt.isdir(pt.dirname(pt.abspath(output_name))):
-            print("Invalid output folder: " + pt.dirname(pt.abspath(output_name)))
-            print("")
-            sys.exit(0)
+    def start(self, clientsocket, message):
+        if message.startswith("magnet:?"):
+            f = self.convert(message, clientsocket, "torrent_tmp")
+            self.download(f, clientsocket)
+        else:
+            send(clientsocket, "Broken Magnet!")
 
+    def convert(self, magnet, clientsocket, output_name=None):
         tempdir = tempfile.mkdtemp()
         ses = lt.session()
         params = {
@@ -56,18 +34,16 @@ class Consumer():
         }
         handle = lt.add_magnet_uri(ses, magnet, params)
 
-        print("Downloading Metadata (this may take a while)")
+        self.send(clientsocket, "Downloading Metadata (this may take a while)")
         while (not handle.has_metadata()):
             try:
                 time.sleep(1)
             except KeyboardInterrupt:
-                print("Aborting...")
                 ses.pause()
-                print("Cleanup dir " + tempdir)
                 shutil.rmtree(tempdir)
                 sys.exit(0)
         ses.pause()
-        print("Done")
+        self.send(clientsocket, "Metadata finished")
 
         torinfo = handle.get_torrent_info()
         torfile = lt.create_torrent(torinfo)
@@ -81,15 +57,15 @@ class Consumer():
             elif pt.isdir(pt.dirname(pt.abspath(output_name))):
                 output = pt.abspath(output_name)
 
-        print("Saving torrent file here " + output + " ...")
+        self.send(clientsocket, "Saving torrent file here " + output + " ...")
         torcontent = lt.bencode(torfile.generate())
         f = open(output, "wb")
         f.write(lt.bencode(torfile.generate()))
         f.close()
-        print("Saved! Cleaning up dir: " + tempdir)
+        self.send(clientsocket, "Saved! Cleaning up dir: " + tempdir)
+
         ses.remove_torrent(handle)
         shutil.rmtree(tempdir)
-
         return output
     
     def download(self, file, clientsocket):
@@ -111,35 +87,30 @@ class Consumer():
             peers_count = s.num_peers
             state = state_str[s.state]
 
-            # print("complete: %.2f, down: %.1f kB/s, up: %.1f kB/s, peers: %d, state: %s" % \
-            # (progress, d_rate, u_rate, peers_count, state),)
-            report = "complete: {0} %, down: {1} kB/s, up: {2} kB/s, peers: {3}, state: {4}".format(progress, d_rate, u_rate, peers_count, state)
-            clientsocket.send(report)
+            report = "complete: {0} %, down: {1} kB/s, up: {2} kB/s, peers: {3}, state: {4}" .format(progress, d_rate, u_rate, peers_count, state)
+            self.send(clientsocket, report)
             time.sleep(1)
 
-        clientsocket.send(h.name() + 'complete')
+        self.send(clientsocket, "Done!")
         time.sleep(3)
 
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serversocket.bind(('', 9999))
 serversocket.listen(5)
-print("waiting for connection..")
-(clientsocket, port) = serversocket.accept()
-print("found one, starting now..")
 
-producer = Producer()
-consumer = Consumer()
-queue = Queue()
+while True:
+    try:
+        print("waiting for connection..")
+        (clientsocket, port) = serversocket.accept()
+        print("found one, starting now..")
 
-t1 = threading.Thread( target=producer.start, args=(clientsocket, queue) )
-t2 = threading.Thread( target=consumer.start, args=(clientsocket, queue) )
+        torrent = Torrent()
+        message = clientsocket.recv(1024)
 
-t1.start()
-t2.start()
-
-t1.join()
-t2.join()
-
-print "server closing.."
-serversocket.close()
+        t = threading.Thread( target=torrent.start, args=(clientsocket, message) )
+        t.start()
+    except KeyboardInterrupt:
+        clientsocket.close()
+        break
